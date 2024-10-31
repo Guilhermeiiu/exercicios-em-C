3f4f07df0ebc2df5a2455fa02ccbf3c5c3bbcc5c
Guilhermeiiu <guicfranca8@gmail.com>
2024-10-31 12:11:51 -0300

    att3

---
+++ 6.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAMANHO 50
+
+void preencherVetor(int vetor[]) {
+    srand(time(NULL)); 
+    for (int i = 0; i < TAMANHO; i++) {
+        vetor[i] = rand() % 101; 
+    }
+}
+
+// Função para ordenar um vetor em ordem crescente (utiliza o algoritmo de bolha)
+void ordenarVetor(int vetor[]) {
+    int i, j, aux;
+    for (i = 0; i < TAMANHO - 1; i++) {
+        for (j = 0; j < TAMANHO - i - 1; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                aux = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = aux; }
+        }
+    }
+}
+
+void imprimirVetor(int vetor[]) {
+    for (int i = 0; i < TAMANHO; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int numeros[TAMANHO];
+
+    preencherVetor(numeros);
+
+    printf("Vetor antes da ordenação:\n");
+    imprimirVetor(numeros);
+
+    ordenarVetor(numeros);
+
+    printf("Vetor após a ordenação:\n");
+    imprimirVetor(numeros);
+
+    return 0;
+}
